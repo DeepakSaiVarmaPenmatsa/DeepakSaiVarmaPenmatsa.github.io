75d5e15d4d25a5bad681d4b3073740a2cb7eaffe
DeepakSaiVarmaPenmatsa <pdsvarma4@gmail.com>
2025-05-18 10:35:17 +0530

    upd

---
+++ a15/index.js
@@ -0,0 +1,16 @@
+const products=[
+    {id:1,name:"Banana",price:50},{
+        id:2,name:"apple",price:30
+    },{
+        id:3,name:"orange",price:80
+    },
+];
+
+const cart={1:7,3:5}
+
+products.map((product) => {
+    if(cart[products.id]){
+        const total=product*price * cart[product.id];
+        console.log(product.id,product.name.product.price,total);
+    }
+});
